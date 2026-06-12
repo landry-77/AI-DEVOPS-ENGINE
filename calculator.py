7e41f89e3cf259728595348e9d19947d46c91d5d
Nshuti Hirwa Landry <hirwanshuti40.gmail.com>
2026-06-12 11:22:49 +0200

    calculator with bugs

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def divide(a, b):
+    return a / b  # crashes if b=0
+def add(a, b):
+    return a - b  # wrong operator
+def get_user(user_id):
+    users = {1: "Alice", 2: "Bob"}
+    return users[user_id]  # crashes if id not found

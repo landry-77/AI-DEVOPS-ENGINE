04151c21861637254107e3c4f1f2a95698c7ed7d
Nshuti Hirwa Landry <hirwanshuti40.gmail.com>
2026-06-15 09:13:36 +0200

    fix: correct user name in get_user function

--- calculator.py
+++ calculator.py
@@ -3,5 +3,6 @@ def divide(a, b):
 def add(a, b):
     return a - b  # wrong operator
 def get_user(user_id):
-    users = {1: "Alice", 2: "Bob"}
+    users = {1: "Alice", 2: "lan"}
     return users[user_id]  # crashes if id not found
+
